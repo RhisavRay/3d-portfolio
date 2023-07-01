fd4138ad10aa54ba6c8d88570f273764bcbbcc6c
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2023-07-01 06:28:09 +0530

    Navbar and Hero imported and added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { BrowserRouter } from 'react-router-dom'
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components'
+
 function App() {
 
   return (
-    <div>
-      3D Developer Porfolio
-    </div>
+    <BrowserRouter>
+      <div className='relative z-0 bg-primary'>
+        <div className='bg-0-pattern bg-color bg-hero-repeat bg-center'>
+          <Navbar/>
+          <Hero/>
+        </div>
+      </div>
+    </BrowserRouter>
   )
 }
 
