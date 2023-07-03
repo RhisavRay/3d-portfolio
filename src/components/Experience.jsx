346938fc33ccebea6a938336f4032617cc0549dd
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2023-07-03 16:50:55 +0530

    ExperienceCard made

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -6,11 +6,38 @@ import { experiences } from '../constants'
 import { SectionWrapper } from '../hoc'
 import { textVariant } from "../utils/motion"
 
-
-const Experience = () => {
-  return (
-    <div>Experience</div>
+const ExperienceCard = ({experience}) =>
+{
+  return(
+    <VerticalTimelineElement contentStyle={{background: '#1D1836', color: '#FFF'}} contentArrowStyle={{borderRight: '7px solid #232631'}} date={experience.date} iconStyle={{background: experience.iconBg}} icon={
+      <div>
+        <img src={experience.icon} alt={experience.company_name}/>
+      </div>
+    } className="w-[60%] h-[60%] object-contain">
+      <div>
+        <h3 className="text-white text-[24px] font-bold">{experience.title}</h3>
+      </div>
+    </VerticalTimelineElement>
   )
 }
 
-export default Experience
+const Experience = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>What I have done so far</p>
+        <h2 className={styles.sectionHeadText}>Work Experience.</h2>
+      </motion.div>
+
+      <div className="mt-20 flex flex-col">
+        <VerticalTimeline>
+          {experiences.map((experience, index) => (
+            <ExperienceCard key={index} experience={experience}/>
+          ))}
+        </VerticalTimeline>
+      </div>
+    </>
+  )
+}
+
+export default SectionWrapper(Experience, 'work')
