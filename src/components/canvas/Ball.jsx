a19b489dfed3a41a6d11055c8bddbb416858feba
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2023-07-03 19:40:07 +0530

    Minor changes to Computer.jsx. Copied Canvas

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -16,7 +16,13 @@ const Ball = (props) =>
 const BallCanvas = ({icon}) =>
 {
   return(
-    <Canvas></Canvas>
+    <Canvas frameloop='demand'dpr={[1, 2]} camera={{ position: [20, 3, 5], fov: 25 }} gl={{ preserveDrawingBuffer: true }}>
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2}/>
+        <Computers isMobile={isMobile} />
+      </Suspense>
+      <Preload all />
+    </Canvas>
   )
 }
 
