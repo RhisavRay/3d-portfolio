2f2b39f0d5e083214d28fc704267eb3e7f3aaf47
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2023-07-03 09:53:34 +0530

    Description added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,11 +3,21 @@ import { Tilt } from 'react-tilt'
 import { motion } from 'framer-motion'
 import { styles } from '../styles'
 import { services } from '../constants'
-import { fadeIn, textVarient } from '../utils/motion'
+import { fadeIn, textVariant } from '../utils/motion'
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+      <motion.p variants={fadeIn("", "", 0.1, 1)}>
+        I'm a skilled software developer with experience in JavaScript, and expertise in frameworks like React.js. I'm
+        a quick learner and work closely with clients to create efficient, scaleable, and user-friendly solutions that
+        solve real-life problems.
+      </motion.p>
+    </>
   )
 }
 
