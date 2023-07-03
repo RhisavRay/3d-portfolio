ce2abd098e7f5060b5262fd670fef9f920e6842e
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2023-07-03 10:06:41 +0530

    Minor adjustments done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,19 +5,18 @@ import { styles } from '../styles'
 import { services } from '../constants'
 import { fadeIn, textVariant } from '../utils/motion'
 
-const ServiceCard = () =>
-{
-  return(
-    <Tilt className="xs:w-[25px] w-full">
-      <motion.div variants={fadeIn("right", "spring", 0.5 * index, 0.75)} className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'>
-        <div options={max: 45, scale: 1, speed: 450} className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
-          <img src={icon} alt={title} className='w-16 h-16 object-contain'/>
-          <h3 className='text-white text-[20px] font-bold text-center'>{title}</h3>
-        </div>
-      </motion.div>
-    </Tilt>
-  )
-}
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div variants={fadeIn("right", "spring", index * 0.5, 0.75)} className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'>
+      <div options={{ max: 45, scale: 1, speed: 450 }} className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'>
+        <img src={icon} alt='web-development' className='w-16 h-16 object-contain'/>
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
 
 const About = () => {
   return (
