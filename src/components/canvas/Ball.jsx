f17b304c6be2cfb42ae06fa8389e97b764bc99ce
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2023-07-03 19:48:48 +0530

    Ball.jsx done

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -2,6 +2,7 @@ import React, { Suspense } from "react"
 import { Canvas } from "@react-three/fiber"
 import { Decal, Float, OrbitControls, Preload, useTexture } from "@react-three/drei"
 import CanvasLoader from "../Loader"
+import { MeshStandardMaterial } from "three"
 
 const Ball = (props) =>
 {
@@ -9,7 +10,15 @@ const Ball = (props) =>
   const [decal] = useTexture([props.imgUrl])
 
   return (
-    <div>Ball</div>
+    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
+      <ambientLight intensity={0.25}/>
+      <directionalLight position={[0, 0, 0.05]}/>
+      <mesh castShadow receiveShadow scale={2.75}>
+        <icosahedronGeometry args={[1, 1]}/>
+        <meshStandardMaterial color='#FFF8EB' polygonOffset polygonOffsetFactor={-5} flatShading/>
+        <Decal map={decal} position={[0, 0, 1]} rotation={[2 * Math.PI, 0, 6.25]} flatShading/>
+      </mesh>
+    </Float>
   )
 }
 
