f39f7e1f2f66d64d8e0b4bc76a324bd1545f9945
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2023-07-03 16:20:04 +0530

    Scrooling part added

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -7,7 +7,10 @@ function HOC()
 {
   return(
     <motion.section variants={staggerContainer()} initial='hidden' whileInView='show' viewport={{once: true, amount: 0.25}} className={`${styles.padding} max-w-7x mx-auto relative z-0`}>
-        <Component/>
+      <span className="hash-span" id={idName}>
+        &nbsp;
+      </span>
+      <Component/>
     </motion.section>
   )
 }
