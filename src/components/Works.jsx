d15d546736c3f4844a15b7b9f6a1401b48968f63
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2023-07-04 07:53:58 +0530

    Imports to Works.jsx done

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,6 +1,13 @@
+import { Tilt } from "react-tilt"
+import { motion } from "framer-motion"
+import { styles } from '../styles'
+import { github } from '../assets'
+import { SectionWrapper } from '../hoc'
+import { projects } from '../constants'
+import { fadeIn, textVariant } from "../utils"
 
-
-const Works = () => {
+const Works = () =>
+{
   return (
     <div>Works</div>
   )
