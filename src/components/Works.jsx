dea624165f82f6f1823e6cbf4b2bc9572f0a81cf
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2023-08-05 10:23:52 +0530

    .

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
-
+import Tilt from "react-tilt";
 import { styles } from "../styles";
 import { github } from "../assets";
 import { SectionWrapper } from "../hoc";
